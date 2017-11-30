3be739529adc4536c25691a6087df0ff6eac8a17
wcrozier12 <wcc8@nau.edu>
2017-11-30 15:24:15 -0700

    changed query to reflect heroku db

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,7 @@ const client = require('../config/connection');
 const pg = require('pg');
 
 router.get('/api/contacts', ((req, res) => {
-  const query = "SELECT * FROM " + '"ContactInfo"' + "." + '"Contacts"';
+  const query = "SELECT * FROM contacts";
   console.log(query);
 
   client.query(query, (err, response) => {
@@ -17,7 +17,7 @@ router.get('/api/contacts', ((req, res) => {
 
 router.get('/search/:firstName/:lastName', ((req, res) => {
   console.log(req.params);
-  const query="SELECT * FROM " + '"ContactInfo"' + "." + '"Contacts" WHERE firstname=' + "'" +  req.params.firstName + "'" + 'OR lastname='  + "'" +  req.params.lastName + "'";
+  const query="SELECT * FROM contacts WHERE firstname=" + "'" +  req.params.firstName + "'" + 'OR lastname='  + "'" +  req.params.lastName + "'";
   console.log(query);
   client.query(query, (err, response) => {
     if(err) {
